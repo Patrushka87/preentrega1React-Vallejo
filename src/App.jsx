b255245afb1718a8da3de5ac499749f84576eeac
unknown <daniel.cero.nueve@gmail.com>
2023-04-29 11:48:19 -0300

    entrega

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './Componentes/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+    <h1>asfa</h1>
+    </>
+  )
+}
+
+export default App
